55c4f76da0fe0b37da77b642b4c71496033826e4
aneCrow <anecrow@outlook.com>
2022-12-04 09:44:22 +0800

    feat: project init

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+    input: `lib/index.ts`,
+    output: [
+        {
+            file: 'dist/index.es.js',
+            format: 'esm'
+        },
+        {
+            file: 'dist/index.iife.js',
+            name: 'Eagle',
+            format: 'iife',
+            sourcemap: true
+        },
+    ],
+    plugins: [typescript({
+        useTsconfigDeclarationDir: true
+    })],
+};
