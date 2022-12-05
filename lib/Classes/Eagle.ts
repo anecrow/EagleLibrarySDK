ac79f288f783f4312934ae82b05c99b51441cdd1
aneCrow <anecrow@outlook.com>
2022-12-06 06:54:03 +0800

    feat: class eagle info move in raw

--- lib/Classes/Eagle.ts
+++ lib/Classes/Eagle.ts
@@ -2,31 +2,26 @@ import API from "../API/index";
 import { ApplicationInfo } from "../API/typs";
 import { ConsoleLog, ConsoleWarn, ConsoleError } from "../Util";
 
-export default class Eagle implements ApplicationInfo {
+export default class Eagle {
   static async CheckServer() {
     try {
       const info = await API.AppInfo();
       ConsoleLog("Eagle is online.", info.version);
-      console.log(info);
+      return true;
     } catch (error) {
       ConsoleError("Check server failed.");
+      return false;
     }
   }
 
-  version: string;
-  prereleaseVersion: string | null;
-  buildVersion: string;
-  execPath: string;
-  platform: string;
+  raw: ApplicationInfo;
+
   constructor(info: ApplicationInfo) {
-    this.version = info.version;
-    this.prereleaseVersion = info.prereleaseVersion;
-    this.buildVersion = info.buildVersion;
-    this.execPath = info.execPath;
-    this.platform = info.platform;
+    this.raw = info;
   }
 
   async updata() {
-    Object.assign(this, await API.AppInfo());
+    this.raw = await API.AppInfo();
+    return this;
   }
 }
