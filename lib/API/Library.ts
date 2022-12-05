c29fdeb086673d54e5e75214bd00bbe13f63ce77
aneCrow <anecrow@outlook.com>
2022-12-06 03:41:04 +0800

    feat: library api

---
+++ lib/API/Library.ts
@@ -0,0 +1,40 @@
+import { fetchGET, fetchPOST } from "./Base";
+import * as Type from "./typs";
+
+/**
+ * **应用版本信息**
+ *
+ * `GET` 取得当前运行 Eagle App 的详细信息，通常我们可以透过这个方式判断用户设备是否能够运行某些功能。
+ */
+export function AppInfo() {
+  return fetchGET("/api/application/info") as Promise<Type.ApplicationInfo>;
+}
+
+/**
+ * **取得资源库信息**
+ *
+ * `GET` 取得当前运行资源库的详细信息，你可以透过这个功能快速取得 所有文件夹 、所有智能文件夹 、 所有标签群组、 快速访问 等信息。
+ */
+export function LibraryInfo() {
+  return fetchGET("/api/library/info") as Promise<Type.LibraryInfo>;
+}
+
+/**
+ * **资源库列表**
+ *
+ * `GET` 取得软件中最近使用过的资源库列表
+ */
+export function LibraryHistory() {
+  return fetchGET("/api/library/history") as Promise<string[]>;
+}
+
+/**
+ * **切换资源库**
+ *
+ * `POST` 切换 Eagle 打开的资源库。
+ */
+export function LibrarySwitch(/** 资源库路径 */ libraryPath: string) {
+  return fetchPOST("/api/library/switch", {
+    libraryPath: libraryPath,
+  }) as Promise<undefined>;
+}
--- lib/API/typs.ts
+++ lib/API/typs.ts
@@ -1,1 +1,31 @@
-export type Data = any;
+export type Data = undefined | string[] | ApplicationInfo | LibraryInfo;
+
+/* ---------------------
+ * Application & Library
+ -------------------- */
+export interface ApplicationInfo {
+  version: string;
+  prereleaseVersion: string | null; //XXX: 不确定
+  buildVersion: string;
+  execPath: string;
+  platform: string;
+}
+export interface LibraryInfo {
+  applicationVersion: string;
+  library: {
+    name: string;
+    path: string;
+  };
+  modificationTime: number;
+  folders: [];
+  smartFolders: [];
+  tagsGroups: {
+    id: string;
+    name: string;
+    tags: string[];
+  }[];
+  quickAccess: {
+    id: string;
+    type: "folder" | "smartFolder";
+  }[];
+}
