f72b20f3a9ee84880db7f31f5412cb5668a3574c
aneCrow <anecrow@outlook.com>
2022-12-06 03:41:50 +0800

    feat: export library api

--- lib/API/index.ts
+++ lib/API/index.ts
@@ -1,4 +1,5 @@
 import * as Base from "./Base";
+import * as Library from "./Library";
 
 namespace API {
   export type URLInfo = Base.APIPath;
@@ -6,5 +7,11 @@ namespace API {
   export const fetchAwaitData = Base.fetchAwaitData;
   export const fetchGET = Base.fetchGET;
   export const fetchPOST = Base.fetchPOST;
+}
+namespace API {
+  export const AppInfo = Library.AppInfo;
+  export const LibraryInfo = Library.LibraryInfo;
+  export const LibraryHistory = Library.LibraryHistory;
+  export const LibrarySwitch = Library.LibrarySwitch;
 }
 export default API;
