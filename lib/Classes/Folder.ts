47e023265bbde489f9b89c9ed25d69227f9fc8e1
aneCrow <anecrow@outlook.com>
2022-12-06 09:51:56 +0800

    feat: class folder add update

--- lib/Classes/Folder.ts
+++ lib/Classes/Folder.ts
@@ -3,8 +3,18 @@ import { CommonFolder } from "../API/typs";
 import { ConsoleLog, ConsoleWarn, ConsoleError } from "../Util";
 
 export default class Folder {
+  
   raw: CommonFolder;
+  get name() {
+    return this.raw.name;
+  }
+
   constructor(info: CommonFolder) {
     this.raw = info;
+  }
+
+  async update() {
+    this.raw = await API.FolderUpdate(this.raw.id);
+    return this;
   }
 }
