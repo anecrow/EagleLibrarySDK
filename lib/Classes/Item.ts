85bd0668f37bd5de47f75ace58b9d869797891e2
aneCrow <anecrow@outlook.com>
2022-12-07 02:19:09 +0800

    feat: add item property

--- lib/Classes/Item.ts
+++ lib/Classes/Item.ts
@@ -5,12 +5,45 @@ import { ConsoleLog, ConsoleWarn, ConsoleError } from "../Util";
 export default class Item {
   raw: ItemInfo;
   name: string;
+  get tags() {
+    return this.raw.tags;
+  }
+  set tags(tags: string[]) {
+    this.update({ tags: tags });
+  }
+  get annotation() {
+    return this.raw.annotation;
+  }
+  set annotation(annotation: string) {
+    this.update({ annotation: annotation });
+  }
+  get url() {
+    return this.raw.url;
+  }
+  set url(url: string) {
+    this.update({ url: url });
+  }
+  get star() {
+    return this.raw.star;
+  }
+  set star(star: number) {
+    this.update({ star: star });
+  }
+
   constructor(info: ItemInfo) {
     this.raw = info;
     this.name = this.raw.name;
   }
-  async update() {
-    this.raw = await API.ItemUpdate(this.raw.id);
+
+  async update(): Promise<void>;
+  async update(option: {
+    /** 标签 */ tags?: string[];
+    /** 注释 */ annotation?: string;
+    /** 来源网址 */ url?: string;
+    /** 评分 */ star?: number;
+  }): Promise<void>;
+  async update(option?: any) {
+    this.raw = await API.ItemUpdate(this.raw.id, option);
     this.name = this.raw.name;
   }
 }
