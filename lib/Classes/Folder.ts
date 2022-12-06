2055e9e0429c3436233f648f7936723afdcba967
aneCrow <anecrow@outlook.com>
2022-12-07 05:06:20 +0800

    feat: folder add funcs

--- lib/Classes/Folder.ts
+++ lib/Classes/Folder.ts
@@ -1,5 +1,5 @@
 import API from "../API/index";
-import { CommonFolder } from "../API/typs";
+import { CommonFolder, IconColor } from "../API/typs";
 import { ConsoleLog, ConsoleWarn, ConsoleError } from "../Util";
 import Library from "./Library"; // XXX:  循环引用
 import Item from "./Item";
@@ -36,6 +36,15 @@ export default class Folder {
       return infoes.map((info) => new Item(info));
     })();
   }
+  set newName(newName: string) {
+    this.update({ newName: newName });
+  }
+  set newDescription(newDescription: string) {
+    this.update({ newDescription: newDescription });
+  }
+  set newColor(newColor: IconColor) {
+    this.update({ newColor: newColor });
+  }
 
   constructor(info: CommonFolder) {
     this.raw = info;
@@ -45,9 +54,47 @@ export default class Folder {
     yield* Folder.Generator(this.children);
   }
 
-  async update() {
-    this.raw = await API.FolderUpdate(this.raw.id);
+  async update(): Promise<Folder>;
+  async update(option: {
+    /** 新的文件夹名 */ newName?: string;
+    /** 新的文件夹描述 */ newDescription?: string;
+    /** 图标颜色 */ newColor?: IconColor;
+  }): Promise<Folder>;
+  async update(option?: any) {
+    this.raw = await API.FolderUpdate(this.raw.id, option);
     this.name = this.raw.name;
     return this;
+  }
+
+  async deleteItems() {
+    const items = await this.items;
+    API.ItemMoveToTrash(items.map((info) => info.raw.id));
+  }
+  async addSubFolder(name: string) {
+    return API.FolderCreate(name, this.raw.id);
+  }
+  ItemAddFromURLs(
+    /** 由多个 item 组成的 array 物件 */ items: {
+      /** 图片链接，支持`http` `https` `base64` */ url: string;
+      /** 图片名 */ name: string;
+      /** 来源网址 */ website?: string;
+      /** 标签 */ tags?: string[];
+      /** 注释 */ annotation?: string;
+      /** 创建时间，可以用此参数控制添加后在 Eagle 的排列顺序 */ modificationTime?: number;
+      /** 选填，自定义 HTTP headers 属性，可用来绕过特定网站保护机制 */ headers?: any;
+    }[]
+  ) {
+    API.ItemAddFromURLs(items, this.raw.id);
+  }
+  ItemAddFromPaths(
+    items: {
+      /** 图片链接，支持`http` `https` `base64` */ url: string;
+      /** 图片名 */ name: string;
+      /** 来源网址 */ website?: string;
+      /** 标签 */ tags?: string[];
+      /** 注释 */ annotation?: string;
+    }[]
+  ) {
+    API.ItemAddFromPaths(items, this.raw.id);
   }
 }
