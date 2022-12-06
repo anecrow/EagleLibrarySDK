3c14658c2d67148578060867998c56c450110070
aneCrow <anecrow@outlook.com>
2022-12-07 01:51:41 +0800

    feat: folder add items

--- lib/Classes/Folder.ts
+++ lib/Classes/Folder.ts
@@ -1,6 +1,7 @@
 import API from "../API/index";
 import { CommonFolder } from "../API/typs";
 import { ConsoleLog, ConsoleWarn, ConsoleError } from "../Util";
+import Item from "./Item";
 
 export default class Folder {
   static *Generator(folders: Folder[]): Generator<Folder, void, undefined> {
@@ -20,6 +21,12 @@ export default class Folder {
   name: string
   get children() {
     return this.raw.children.map((info) => new Folder(info));
+  }
+  get items() {
+    return (async () => {
+      const infoes = await API.ItemList({ folders: [this.raw.id] });
+      return infoes.map((info) => new Item(info));
+    })();
   }
 
   constructor(info: CommonFolder) {
